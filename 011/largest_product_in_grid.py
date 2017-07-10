6a1495feb5ac9403b2deac89b143aa2e4c4a310f
Maria Laura Staffini <mls54@st-andrews.ac.uk>
2017-07-10 14:51:49 +0100

    011: Edit for a neater version of functions

--- 011/largest_product_in_grid.py
+++ 011/largest_product_in_grid.py
@@ -2,36 +2,27 @@
 
 import numpy as np
 
-mat=np.genfromtxt("grid_dummy.txt",delimiter=" ")
+mat=np.genfromtxt("grid.txt",delimiter=" ")
 
 xmax=mat.shape[0]-1
 ymax=mat.shape[1]-1
 
 xstart=ystart=0
 
-# horizontal
-def horizontal(mat, x, y):
-    return mat[x,y]*mat[x,y+1]*mat[x,y+2]*mat[x,y+3]
-    
-# vertical
-def vertical(mat, x, y):
-    return mat[x,y]*mat[x+1,y]*mat[x+2,y]*mat[x+3,y]
-
-# diagonal TL to BR
-def diagTLBR(mat, x, y):
-    return mat[x,y]*mat[x+1,y+1]*mat[x+2,y+2]*mat[x+3,y+3]
-
-# diagonal TR to BL
-def diagTRBL(mat, x, y):
-    return mat[x,y]*mat[x+1,y-1]*mat[x+2,y-2]*mat[x+3,y-3]
+def span(xstart,ystart,xfactor,yfactor,num):
+    ''' Start at [xstart,ystart] and get the product of num consecutive
+    entries in a given direction specified by factors:
+    (+1,0) vertical, (0,+1) horizontal, (+1,+1) top left to bottom right,
+    (+1,-1) top right to bottom left. '''
+    return np.product([mat[xstart+z*xfactor,ystart+z*yfactor] for z in range(num)])
 
 current_max=0
 for i in range(xmax+1):
     for j in range(ymax+1):
         if i<xmax-2:
-            if j>2: current_max=max(diagTRBL(mat,i,j),current_max)
-            if j<ymax-2: current_max=max(diagTLBR(mat,i,j),current_max)
-            current_max=max(vertical(mat,i,j),current_max)
-        if j<ymax-2: current_max=max(horizontal(mat,i,j),current_max)
+            if j>2: current_max=max(span(i,j,+1,-1,4),current_max)
+            if j<ymax-2: current_max=max(span(i,j,+1,+1,4),current_max)
+            current_max=max(span(i,j,+1,0,4),current_max)
+        if j<ymax-2: current_max=max(span(i,j,0,+1,4),current_max)
                    
 print int(current_max)
