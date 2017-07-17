f06f8b791ec0c1e172f5b847d17f5a81c93b3e5e
Maria Laura Staffini <mls54@st-andrews.ac.uk>
2017-07-17 14:54:34 +0100

    012: Edit for faster method by generated prime list

--- 012/triangle_number_factors.py
+++ 012/triangle_number_factors.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from timeit import default_timer as timer
+
 num=1
 # We could assume something more about starting point really,
 # however we definitely know 7th doesn't have 500...
@@ -21,11 +23,42 @@ def divisors(n):
 def triangle(n):
     return n*(n+1)*0.5
 
+#~~ METHOD 2 ~~~#
+
+def primes_up_to(upto):
+    primes=[2]
+    for n in range(3,upto+1):
+        for m in range(0,len(primes)):
+            if primes[m]<=n**0.5 and n % primes[m] == 0: break
+            elif primes[m]>n**0.5:
+                primes+=[n]
+                break
+    return primes
+
+
+def divisors_using_gen_primes(n,primes):
+    fact=1
+    for i in range(0, len(primes)):
+        exp=1
+        while n%primes[i]==0:
+            exp+=1
+            n/=primes[i]
+        fact*=exp
+    return fact
+
 #~~~~~~~~~~~~~~~~~~~~~#
 
+start=timer()
+
+primes=primes_up_to(1000)
+
 current=triangle(num)
-while divisors(current)<lim:
+#while divisors(current)<lim:
+while divisors_using_gen_primes(current,primes)<lim:
     num+=1
     current=triangle(num)
 
 print int(current)
+
+end=timer()
+print "Time elapsed ", end-start
